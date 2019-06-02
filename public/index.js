fc77f931ca70c65d39da1c14de0cab9f311870ed
hirschet <ethan@hirschfamily.com>
2019-06-02 12:45:01 -0700

    added functionality and formatting to donate modal

--- public/index.js
+++ public/index.js
@@ -1,1 +1,23 @@
 console.log("JS LOADED");
+
+
+// DONATE MODAL
+
+var donateCat = document.getElementById('donate');
+
+var donateModalBG = document.getElementById('modal-backdrop');
+
+var donateModal = document.getElementById('add-cat-modal');
+
+var cancelButton = document.getElementsByClassName('modal-cancel-button')[0];
+
+function closeModal(event){
+  donateModal.classList.toggle('hidden');
+  donateModalBG.classList.toggle('hidden');
+}
+
+donateCat.addEventListener('click', closeModal);
+
+cancelButton.addEventListener('click', closeModal);
+
+// END OF DONATE MODAL
