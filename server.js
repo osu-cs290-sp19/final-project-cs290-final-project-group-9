90c3bea321dc95d2d42f51fbf8d41d00b1b1bdbf
ruangroc <ruangroc@oregonstate.edu>
2019-06-06 18:01:55 -0700

    added initial MongoDB stuff in server.js

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ var express = require('express');
 var handlebars = require('handlebars');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var port = process.env.PORT || 3330;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoUrl = 'mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}';
+var db = null;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -64,12 +74,16 @@ app.get('*', function(req, res, next) {
     res.status(404).render('404');
 })
 
-
-//Listener
-app.listen(port, function(err) {
+//Listener and database setup
+MongoClient.connect(mongoUrl, function (err, client) {
     if (err) {
         throw err;
     }
-    console.log("== Server is listening on port", port);
+    db = client.db(mongoDBName);
+    app.listen(port, function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log("== Server is listening on port", port);
+    });
 });
-
